5b390c69e9b4c0f5024754df6435bfccf2b5502f
Ивайло Шопов <is41062491@edu.mon.bg>
2023-07-10 16:43:21 +0300

    Add project files.

---
+++ 2. Ascii Sumator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _2._Ascii_Sumator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstChar = char.Parse(Console.ReadLine());
+            char lastChar = char.Parse(Console.ReadLine());
+            string textOfChars = Console.ReadLine();
+            char[] textToCharArray = textOfChars.ToCharArray();
+            int totalSumOfASCIICodes = 0;
+            foreach  (char character in textToCharArray)
+            {
+                int firstCharASCIICode = firstChar;
+                int lastCharASCIICode = lastChar;
+                int currentCharASCIICode = character;
+                if (currentCharASCIICode>firstCharASCIICode && currentCharASCIICode<lastCharASCIICode)
+                {
+                    totalSumOfASCIICodes += currentCharASCIICode;
+                }
+            }
+            Console.WriteLine(totalSumOfASCIICodes);
+        }
+    }
+}
---
+++ 4. Morse Code Translator/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MorseCodeTranslator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string morseCode = Console.ReadLine();
+
+            string[] words = morseCode.Split(new[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
+            StringBuilder translatedMessage = new StringBuilder();
+
+            foreach (string word in words)
+            {
+                string[] letters = word.Split(' ');
+                StringBuilder translatedWord = new StringBuilder();
+
+                foreach (string letter in letters)
+                {
+                    if (MorseCodeDictionary.ContainsKey(letter))
+                    {
+                        char translatedLetter = MorseCodeDictionary[letter];
+                        translatedWord.Append(translatedLetter);
+                    }
+                }
+
+                translatedMessage.Append(translatedWord).Append(' ');
+            }
+
+            string finalMessage = translatedMessage.ToString().Trim();
+            Console.WriteLine(finalMessage);
+        }
+
+        static Dictionary<string, char> MorseCodeDictionary = new Dictionary<string, char>()
+        {
+            {".-", 'A'},
+            {"-...", 'B'},
+            {"-.-.", 'C'},
+            {"-..", 'D'},
+            {".", 'E'},
+            {"..-.", 'F'},
+            {"--.", 'G'},
+            {"....", 'H'},
+            {"..", 'I'},
+            {".---", 'J'},
+            {"-.-", 'K'},
+            {".-..", 'L'},
+            {"--", 'M'},
+            {"-.", 'N'},
+            {"---", 'O'},
+            {".--.", 'P'},
+            {"--.-", 'Q'},
+            {".-.", 'R'},
+            {"...", 'S'},
+            {"-", 'T'},
+            {"..-", 'U'},
+            {"...-", 'V'},
+            {".--", 'W'},
+            {"-..-", 'X'},
+            {"-.--", 'Y'},
+            {"--..", 'Z'}
+        };
+    }
+}
